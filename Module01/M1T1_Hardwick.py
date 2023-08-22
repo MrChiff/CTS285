0201e98ed4beb66813d123750fee7f60337667b5
MrChiff <105020263+MrChiff@users.noreply.github.com>
2023-08-22 19:16:53 -0400

    Create M1T1_hardwick

---
+++ Module01/M1T1_Hardwick.py
@@ -0,0 +1,18 @@
+# CTS 285
+# M1T1
+# Jonathan Hardwick
+# 23/08/23
+
+# The program should print at least three lines of text when run (70 
+# points).
+#   - Your first and last name
+#   - What languages you are currently studying this semester
+#   - Any additional information about yourself you'd like to volunteer 
+#     -- job, hobbies, interests, etc.
+#   - If you are going to be completing the first Homework with a 
+#     partner, then list your partner's name.
+
+print("Jonathan Hardwick\n")
+print("I am not currently studying any languages this semester but have studied Fortran, JAVA, Python, and a little C++ in the previous semesters.\n")
+print("I am a rocket scientist and I am very enthusiastic about all things IT!\n")
+print("Homework partner:  TBA\n")
